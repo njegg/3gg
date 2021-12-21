64572ce022d65e039a100bdc78d5b857d443010c
nyegomir <njegomir15@gmail.com>
2021-12-21 21:29:07 +0100

    rayz work!

--- src/game/Main.java
+++ src/game/Main.java
@@ -15,8 +15,6 @@ import mars.input.InputEvent;
 import mars.input.InputState;
 import mars.utils.Numeric;
 
-import javax.swing.*;
-
 
 public class Main implements Drawing {
 
@@ -42,11 +40,6 @@ public class Main implements Drawing {
     int limit = 0;
 
     public void draw(View view) {
-        if (limit++ > 150) {
-            limit = 0;
-//            System.out.printf("%.2f\n", player.angle * 360);
-        }
-
         DrawingUtils.clear(view, Color.gray(0.125));
 
         drawMap(view);
@@ -103,35 +96,41 @@ public class Main implements Drawing {
         Vector vHit = new Vector(2*mapSize, 2*mapSize);
         Vector hHit = new Vector(2*mapSize, 2*mapSize);
 
-        int mapX = 0, mapY = 0, depth;
-        double x, y, xOffset, yOffset, rayX, rayY;
+        int mapX, mapY, depth;
+        double xOffset, yOffset, rayX, rayY;
 
-        double tan = Numeric.tanT(player.angle);
-        double ctan = 1 / tan;
+        double angle = player.angle - 0.05;
+        double dAngle = 0.1;
 
-        double mody64 = Numeric.mod(player.p.y, gridSize);
+        if (angle >  1) angle %= 1;
+        if (angle <  0) angle = 1 + angle;
 
-        for (int i = 0; i < 1; i++) {
-            if (player.angle < 0.5) { // looking up
-                rayY = gridSize - mody64;
+        double mod64 = Numeric.mod(player.p.y, gridSize);
+
+        for (int i = 0; i < 2; i++) {
+            double tan = Numeric.tanT(angle);
+            double ctan = 1.0 / tan;
+
+            /* vertical rays */
+
+            boolean up = false;
+            if (angle < 0.5) {       // looking up
+                up = true;
+                rayY = gridSize - mod64;
                 yOffset = gridSize;
-            } else { // looking down
-                rayY = - mody64;
+            } else {
+                rayY = - mod64;
                 yOffset = - gridSize;
             }
             rayX = rayY * ctan;
-
             xOffset = yOffset * ctan;
 
-
             depth = 0;
-            if (player.angle == 0 || player.angle == 0.5) depth = 8;
+            if (angle == 0 || angle == 0.5) depth = 8;
 
             while (depth < 8) {
                 mapX = (int) ((rayX + player.p.x) / gridSize);
-                mapY = (int) ((rayY + player.p.y) / gridSize); //  - (player.angle < 0.5 ? 0 : 1);
-
-                System.out.println(rayX + "   " + rayY);
+                mapY = (int) ((rayY + player.p.y) / gridSize) - (up ? 0 : 1);
 
                 if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid && map[mapY][mapX] == 1) {
                     break;
@@ -142,34 +141,31 @@ public class Main implements Drawing {
                 }
             }
 
-            if (depth < 9) {
-                view.setFill(Color.CORNFLOWERBLUE);
-                vHit = Vector.vec(rayX + player.p.x, player.p.y + rayY);
-//                view.fillCircleCentered(vHit, 5);
-            }
+            vHit = Vector.vec(rayX + player.p.x, player.p.y + rayY);
 
-            /*  */
+            /* horizontal rays */
 
-            mody64 = Numeric.mod(player.p.x, gridSize);
+            mod64 = Numeric.mod(player.p.x, gridSize);
 
-            if (player.angle < 0.25 || player.angle > 0.75) { // looking up
-                rayX = gridSize - mody64;
+            boolean right = false;
+
+            if (angle < 0.25 || angle > 0.75) { // looking right
+                right = true;
+                rayX = gridSize - mod64;
                 xOffset = gridSize;
-            } else { // looking down
-                rayX = - mody64;
-                xOffset = - gridSize;
+            } else {
+                rayX = -mod64;
+                xOffset = -gridSize;
             }
             rayY = rayX * tan;
-
             yOffset = xOffset * tan;
 
-
             depth = 0;
-            if (player.angle == 0.25 || player.angle == 0.75) depth = 8;
+            if (angle == 0.25 || angle == 0.75) depth = 8;
 
             while (depth < 8) {
-                mapX = (int) ((rayX + player.p.x) / gridSize);
-                mapY = (int) ((rayY + player.p.y) / gridSize); //  - (player.angle < 0.5 ? 0 : 1);
+                mapX = (int) ((rayX + player.p.x) / gridSize) - (right ? 0 : 1);
+                mapY = (int) ((rayY + player.p.y) / gridSize);
 
                 if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid && map[mapY][mapX] == 1) {
                     break;
@@ -180,139 +176,21 @@ public class Main implements Drawing {
                 }
             }
 
-            if (depth < 9) {
-                view.setFill(Color.CORNFLOWERBLUE);
-                hHit = Vector.vec(rayX + player.p.x, player.p.y + rayY);
-//                view.fillCircleCentered(hHit, 5);
-            }
+            hHit = Vector.vec(rayX + player.p.x, player.p.y + rayY);
 
             double hDist = hHit.distanceTo(player.p);
             double vDist = vHit.distanceTo(player.p);
 
             Vector ray = hDist < vDist ? hHit : vHit;
 
-            view.setStroke(Color.RED);
-            view.setFill(Color.CORNFLOWERBLUE);
+            view.setStroke(Color.ORANGE);
             view.strokeLine(player.p, ray);
 
+            angle += dAngle;
+            if (angle >  1) angle %= 1;
+            if (angle <  0) angle = 1 + angle;
         }
     }
-
-//    public void drawRejzOld(View view) {
-//        view.setStroke(Color.CORNFLOWERBLUE);
-//
-//        view.stateStore();
-//
-//        view.setTransformation(Transformation.translation(Vector.vec(-mapSize/2, -mapSize/2)));
-//
-//        Vector vHit, hHit;
-//
-//        int mapX = 0, mapY = 0, depth;
-//        double x, y, xOffset, yOffset, rayX, rayY;
-//
-//        // TODO use normal system, but translate view after rendering
-//
-//        double tan = Numeric.tanT(player.angle);
-//        double ctan = 1 / tan;
-//
-//        for (int i = 0; i < 1; i++) {
-//
-//            /* vertical rays */
-//
-//            boolean up;
-//            if (player.angle < 0.5) { // looking up
-//                up = true;
-//                y = 64 - Numeric.mod(player.p.y , 64);
-//            } else { // looking down
-//                up = false;
-//                y = -Numeric.mod(player.p.y, 64);
-//                mapOffsetY -= gridSize;
-//            }
-//
-//            x = y * ctan;
-//
-//            depth = 0;
-//            if  (player.angle == 0 || player.angle == 0.5) depth = 8;
-//
-//            yOffset = 64 * (up ? 1 : -1);
-//
-//            rayX = x;
-//            rayY = y;
-//            while (depth < 8) {
-//                mapX = (int)((rayX + mapOffsetX) / gridSize);
-//                mapY = (int)((rayY + mapOffsetY) / gridSize);
-//
-//                if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid) {
-//                    if (map[mapY][mapX] == 1) {
-//                        break;
-//                    }
-//                }
-//
-//                rayY += yOffset;
-//                rayX += rayY * ctan;
-//
-//                depth++;
-//            }
-//
-//            vHit = Vector.vec(player.p.x + rayX, player.p.y + rayY);
-//
-//
-//            /* horizontal rays */
-//
-//            boolean right;
-//
-//            if (player.angle < 0.25 || player.angle > 0.75) { // looking right
-//                right = true;
-//                x = 64 - Numeric.mod(player.p.x , 64);
-//            } else {
-//                right = false;
-//                x = -1.0 * Numeric.mod(player.p.x, 64);
-//                mapOffsetX -= gridSize;
-//            }
-//
-//            y = x * tan;
-//
-//            depth = 0;
-//            if  (player.angle == 0.25 || player.angle == 0.75) depth = 8;
-//
-//            xOffset = 64 * (right ? 1 : -1);
-//
-//            rayX = x;
-//            rayY = y;
-//            while (depth < 8) {
-//                mapX = (int)((rayX + mapOffsetX) / 64);
-//                mapY = (int)((rayY + mapOffsetY) / 64);
-//
-//                if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid) {
-//                    if (map[mapY][mapX] == 1) {
-//                        break;
-//                    }
-//                }
-//
-//                rayX += xOffset;
-//                rayY += tan * rayX;
-//
-//                depth++;
-//
-////            }
-//
-//            hHit = Vector.vec(player.p.x + rayX, player.p.y + rayY);
-//
-//            double distH = player.p.distanceTo(hHit);
-//            double distV = player.p.distanceTo(vHit);
-//
-//            Vector closestHit = distH < distV ? hHit : vHit;
-//
-////            view.strokeLine(player.p, vHit);
-////            view.setStroke(Color.RED);
-////            view.strokeLine(player.p, hHit);
-//
-//            view.strokeLine(player.p, closestHit);
-//
-//        }
-//
-//        view.stateRestore();
-//    }
 
 
     public static void main(String[] args) {
@@ -339,16 +217,16 @@ public class Main implements Drawing {
             player.v = Vector.vec(speed * isPressed, 0).rotate(player.angle);
         }
 
-//        if (event.isKey(KeyCode.A)) {
-        if (event.isKeyPress(KeyCode.A)) {
-//            player.toRotate = state.keyPressed(KeyCode.A) ? 1 : 0;
-            player.rotate(true);
+        if (event.isKey(KeyCode.A)) {
+//        if (event.isKeyPress(KeyCode.A)) {
+            player.toRotate = state.keyPressed(KeyCode.A) ? 1 : 0;
+//            player.rotate(true);
         }
 
-//        if (event.isKey(KeyCode.D)) {
-        if (event.isKeyPress(KeyCode.D)) {
-//            player.toRotate = state.keyPressed(KeyCode.D) ? -1 : 0;
-            player.rotate(false);
+        if (event.isKey(KeyCode.D)) {
+//        if (event.isKeyPress(KeyCode.D)) {
+            player.toRotate = state.keyPressed(KeyCode.D) ? -1 : 0;
+//            player.rotate(false);
         }
     }
 }
@@ -367,13 +245,15 @@ class Player {
 
     public void update() {
         p = p.add(v);
-//        angle += toRotate * 0.002;
+        angle += toRotate * 0.002;
+        if (angle >  1) angle %= 1;
+        if (angle <  0) angle = 1 + angle;
     }
 
     public void rotate(boolean left) {
         angle += 0.04 * (left ? 1 : -1);
-        if (angle >= 1) angle = 0;
-        if (angle <  0) angle = 1;
+        if (angle >  1) angle %= 1;
+        if (angle <  0) angle = 1 + angle;
     }
 
 }
