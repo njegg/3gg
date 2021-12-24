7ccb839c114d69dee339230ab7b4b34de71a0cc6
nyegomir <njegomir15@gmail.com>
2021-12-24 21:05:18 +0100

    better movement, fps no longer impacts speed and stuff, resisable nxn map, openable doors in different color

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,6 @@
 package game;
 
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import mars.drawingx.application.DrawingApplication;
@@ -15,15 +16,14 @@ import mars.geometry.Transformation;
 import mars.geometry.Vector;
 import mars.input.InputEvent;
 import mars.input.InputState;
+import mars.time.Profiler;
+import mars.time.ProfilerPool;
 import mars.utils.Numeric;
 
 /*
  * TODO
  *  fix:
- *    fps
- *    consistent screen
  *  textures
- *    doors
  *  walking animation (sin/cos screen offset?)
  *  particles:
  *    shooting
@@ -33,23 +33,88 @@ import mars.utils.Numeric;
 
 public class Main implements Drawing {
 
-    int[][] map = {
-            {1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 0, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 0, 1},
-            {1, 0, 0, 1, 1, 1, 0, 1},
-            {1, 0, 0, 1, 0, 0, 0, 1},
-            {1, 0, 0, 1, 1, 0, 0, 1},
-            {1, 0, 0, 0, 0, 0, 0, 1},
-            {1, 1, 1, 1, 1, 1, 1, 1},
+// /*
+     int[][] map = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 2, 0, 2, 0, 2, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1},
     };
-    int nGrid = 8;
+// */
+
+//    int[][] map = {
+//            {1, 1, 1, 1, 1, 1, 1, 1},
+//            {1, 0, 0, 1, 0, 1, 0, 1},
+//            {1, 0, 0, 0, 0, 1, 0, 0},
+//            {1, 0, 0, 0, 0, 0, 0, 0},
+//            {1, 0, 1, 1, 2, 1, 1, 0},
+//            {1, 1, 1, 0, 0, 0, 0, 1},
+//            {1, 0, 2, 0, 0, 0, 0, 1},
+//            {1, 1, 1, 1, 1, 1, 1, 1},
+//    };
+
+    int nGrid = map.length;
     int gridSize = 64;
-    int mapSize = 512;
+    int mapSize = 64 * nGrid;
+
+    Vector mapStart = Vector.vec(-256, -256);
+
+    static class Player {
+        Vector p;
+        double angle;
+        double speed = 2;
+
+        int toRotate;
+        boolean canMove;
+        boolean forward;
+        boolean back;
+        boolean canForward;
+        boolean canBackwards;
+
+        boolean lookingUp;
+        boolean lookingRight;
+
+        boolean signalE;
+
+        double frameDiff;
+
+        public Player(Vector p) {
+            this.p = p;
+            canMove = true;
+        }
+
+        public void update() {
+            if (canForward && forward) {
+                p = p.add(Vector.polar(speed * frameDiff, angle));
+            }
+            if (canBackwards && back) {
+                p = p.sub(Vector.polar(speed * frameDiff, angle));
+            }
+
+            if (toRotate != 0) {
+                angle += toRotate * 0.01 * frameDiff;
+                if (angle >  1) angle %= 1;
+                if (angle <  0) angle += 1;
+
+                lookingUp = angle < 0.5;
+                lookingRight = angle < 0.25 || angle > 0.75;
+            }
+        }
+    }
 
     Player player = new Player(Vector.vec(2 * gridSize, 2 * gridSize));
-    double speed = 2;
-
     boolean twoD = false;
 
     @GadgetInteger
@@ -58,39 +123,50 @@ public class Main implements Drawing {
     @GadgetDoubleLogarithmic(min = 0.0001, max = 0.01)
     double dAngle = 0.005;
 
-    int limit = 0;
+    Profiler profiler = new Profiler("profiler");
+
+    long frameCur, framePrev, frameDiff;
+    @Override
+    public void init(View view) { framePrev = System.currentTimeMillis(); }
+
     public void draw(View view) {
+        frameCur = System.currentTimeMillis();
+        frameDiff = frameCur - framePrev;
+        framePrev = frameCur;
+
+        // more frames = slower (smaller diff), less frames = faster (bigger diff)
+        player.frameDiff = frameDiff * 0.08;
+
         DrawingUtils.clear(view, Color.gray(0.125));
 
         player.update();
+        player.canForward = !collidesWithWallFront();
+        player.canBackwards = !collidesWithWallBack();
 
         if (twoD) {
             drawMap(view);
-        } else {
-            view.setFill(Color.RED);
-            view.setStroke(Color.RED);
-            view.setLineWidth(5);
-
-            view.strokeLine(player.p, player.p.add(Vector.vec(10,  0).rotate(player.angle)));
-            view.fillCircleCentered(player.p, 5);
         }
+
         rejz(view);
 
+        view.setFill(Color.WHITE);
+        view.fillText("PRESS [C] TO TOGGLE PERSPECTIVE", Vector.vec(-300, -300));
     }
 
-
     public void drawMap(View view) {
-        view.setFill(Color.WHITE);
+
         view.setStroke(Color.GRAY);
         view.setLineWidth(1);
 
         view.stateStore();
-
-        view.setTransformation(Transformation.translation(Vector.vec(-mapSize/2, -mapSize/2)));
+        view.setTransformation(Transformation.translation(mapStart));
 
         for (int i = 0; i < nGrid; i++) {
             for (int j = 0; j < nGrid; j++) {
-                if (map[i][j] == 1) {
+                if (map[i][j] == 1) view.setFill(Color.WHITE);
+                else                view.setFill(Color.BLUE);
+
+                if (map[i][j] > 0) {
                     view.fillRect(
                             Vector.vec(j * gridSize, i * gridSize),
                             Vector.vec(gridSize, gridSize)
@@ -112,16 +188,12 @@ public class Main implements Drawing {
 
     public void rejz(View view) {
         view.stateStore();
-
-        view.setStroke(Color.CORNFLOWERBLUE);
-        view.setTransformation(Transformation.translation(Vector.vec(-mapSize/2, -mapSize/2)));
+        view.setTransformation(Transformation.translation(mapStart));
 
         Vector vHit, hHit;
-//
-//        int rajz = 100;
-//        double dAngle = 0.001;
 
-        double angle = player.angle - (dAngle * rayz / 2);
+        double angle = player.angle + (dAngle * rayz / 2);
+        double dist;
 
         if (angle >  1) angle %= 1;
         if (angle <  0) angle += 1;
@@ -132,21 +204,45 @@ public class Main implements Drawing {
 
             double hDist = hHit.distanceTo(player.p);
             double vDist = vHit.distanceTo(player.p);
-            double dist = Math.min(vDist, hDist);
+            dist = Math.min(vDist, hDist);
+            Vector ray = hDist < vDist ? hHit : vHit;
 
             if (twoD) {
-                Vector ray = hDist < vDist ? hHit : vHit;
                 view.setStroke(Color.ORANGE);
                 view.strokeLine(player.p, ray);
             } else {
-                double wallH = mapSize * 15 / dist;
-                double x = i*10-50; // x pos of wall
+
+                // angle between player and ray
+                double PRangle = angle - player.angle;
+                if (PRangle >  1) PRangle %= 1;
+                if (PRangle <  0) PRangle += 1;
+
+                // side rays are longer -> walls left and right are smaller
+                // this scales them to be the same as the distance from
+                // player to wall (middle ray)
+                // doesnt fix short rays?
+                double scale = Numeric.cosT(PRangle);
+
+                double wallH = 512 * 15 / (dist * scale);
+                double x = i*10-40; // x pos of wall
 
                 Vector wallBottom = Vector.vec(x, -wallH + 300);
                 Vector wallTop = Vector.vec(x, wallH + 300);
 
+                double hue = 0;
+                Vector im = toMapCoords(ray);
+                int mapX = (int) im.x;// + (player.lookingRight ? 0 : 1);
+                int mapY = (int) im.y;// + (player.lookingUp ? 0 : 1);
+
+                mapX -= (!lookingRight(angle) && vDist > hDist) ? 1 : 0;
+                mapY -= (!lookingUp(angle)    && vDist < hDist) ? 1 : 0;
+
+                if (inMap(mapX, mapY) && map[mapY][mapX] == 2) {
+                    hue = 240;
+                }
+
                 view.setLineWidth(10);
-                view.setStroke(Color.hsb(0, 1, dist == vDist ? 1 : 0.7));
+                view.setStroke(Color.hsb(hue, 1, dist == vDist ? 1 : 0.7));
                 view.strokeLine(wallBottom, wallTop);
 
                 view.setStroke(Color.gray(0.1));
@@ -156,12 +252,27 @@ public class Main implements Drawing {
                 view.strokeLine(wallTop, Vector.vec(x, 600));
             }
 
-            angle += dAngle;
+            angle -= dAngle;
             if (angle >  1) angle %= 1;
             if (angle <  0) angle += 1;
         }
+
+        view.stateRestore();
     }
 
+    public boolean lookingUp(double angle) {
+        return angle < 0.5;
+    }
+
+    public boolean lookingRight(double angle) {
+        return angle < 0.25 || angle > 0.75;
+    }
+
+    private Vector toMapCoords(Vector v) {
+        return Vector.vec((int) (v.x / gridSize), (int) (v.y / gridSize));
+    }
+
+    // mixed up horisontal and vertical (again) and lazy to fix
     private Vector hHit(double angle) {
         int mapX, mapY, depth;
         double xOffset, yOffset, rayX, rayY;
@@ -188,7 +299,7 @@ public class Main implements Drawing {
             mapX = (int) ((rayX + player.p.x) / gridSize) - (right ? 0 : 1);
             mapY = (int) ((rayY + player.p.y) / gridSize);
 
-            if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid && map[mapY][mapX] == 1) {
+            if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid && map[mapY][mapX] > 0) {
                 break;
             } else {
                 rayY += yOffset;
@@ -226,7 +337,7 @@ public class Main implements Drawing {
             mapX = (int) ((rayX + player.p.x) / gridSize);
             mapY = (int) ((rayY + player.p.y) / gridSize) - (up ? 0 : 1);
 
-            if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid && map[mapY][mapX] == 1) {
+            if (mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid && map[mapY][mapX] > 0) {
                 break;
             } else {
                 rayY += yOffset;
@@ -238,6 +349,41 @@ public class Main implements Drawing {
         return Vector.vec(rayX + player.p.x, player.p.y + rayY);
     }
 
+    public boolean collidesWithWallFront() {
+        double colideLength = 20;
+        int mapX, mapY;
+
+        Vector collider = player.p.add(Vector.polar(colideLength, player.angle));
+
+        mapX = (int) (collider.x / gridSize);
+        mapY = (int) (collider.y / gridSize);
+        boolean colides = collidesWithWall(mapX, mapY);
+
+        if (inMap(mapX, mapY) && map[mapY][mapX] == 2 && player.signalE) { // open door
+            map[mapY][mapX] = 0;
+        }
+
+        return colides;
+    }
+
+    public boolean collidesWithWallBack() {
+        double colideLength = 20;
+        int mapX, mapY;
+
+        Vector collide = player.p.sub(Vector.polar(colideLength, player.angle));
+
+        mapX = (int) (collide.x / gridSize);
+        mapY = (int) (collide.y / gridSize);
+        return collidesWithWall(mapX, mapY);
+    }
+
+    private boolean collidesWithWall(int mapX, int mapY) {
+        return inMap(mapX, mapY) && map[mapY][mapX] > 0;
+    }
+
+    private boolean inMap(int mapX, int mapY) {
+        return mapY >= 0 && mapY < nGrid && mapX >= 0 && mapX < nGrid;
+    }
 
     public static void main(String[] args) {
         Options options = new Options();
@@ -253,51 +399,30 @@ public class Main implements Drawing {
 
     @Override
     public void receiveEvent(View view, InputEvent event, InputState state, Vector pointerWorld, Vector pointerViewBase) {
-        if (event.isKey(KeyCode.W)) {
-            int isPressed = state.keyPressed(KeyCode.W) ? 1 : 0;
-            player.v = Vector.vec(speed * isPressed, 0).rotate(player.angle);
+        if (event.isKeyPress(KeyCode.W)) {
+            player.forward = true;
+        }
+
+        if (event.isKeyRelease(KeyCode.W)) {
+            player.forward = false;
         }
 
         if (event.isKey(KeyCode.S)) {
-            int isPressed = state.keyPressed(KeyCode.S) ? -1 : 0;
-            player.v = Vector.vec(speed * isPressed, 0).rotate(player.angle);
+            player.back = state.keyPressed(KeyCode.S);
         }
 
         if (event.isKey(KeyCode.A)) {
-//        if (event.isKeyPress(KeyCode.A)) {
             player.toRotate = state.keyPressed(KeyCode.A) ? 1 : 0;
-//            player.rotate(true);
         }
 
         if (event.isKey(KeyCode.D)) {
-//        if (event.isKeyPress(KeyCode.D)) {
             player.toRotate = state.keyPressed(KeyCode.D) ? -1 : 0;
-//            player.rotate(false);
         }
 
         if (event.isKeyPress(KeyCode.C)) {
             twoD = !twoD;
         }
 
-    }
-}
-
-class Player {
-    Vector p;
-    Vector v;
-    double angle;
-    int toRotate;
-
-    public Player(Vector p) {
-        this.p = p;
-        v = Vector.ZERO;
-        angle = 0;
-    }
-
-    public void update() {
-        p = p.add(v);
-        angle += toRotate * 0.003;
-        if (angle >  1) angle %= 1;
-        if (angle <  0) angle += 1;
+        player.signalE = event.isKeyPress(KeyCode.E);
     }
 }
